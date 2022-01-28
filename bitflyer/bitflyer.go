ca3055d323da3d5faa791a65994e6a8c4738642c
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-01-28 23:59:47 +0900

    ♻️: add rafactor

--- bitflyer/bitflyer.go
+++ bitflyer/bitflyer.go
@@ -1,7 +1,15 @@
 package bitflyer
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"strconv"
+	"time"
 )
 
 const baseURL = "https://api.bitflyer.com/v1"
@@ -12,7 +20,34 @@ type APIClient struct {
 	httpClient *http.Client // ストラクトのポインタ型？ -> これをどう使おうと思える頭になるのか？
 }
 
-// api認証のためのheaderをつくる
+// headerを追加する処理
 func(api APIClient) header(method, endpoint string, body []byte) map[string]string {
-	timestamp := strconv.
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	log.Println(timestamp)
+	
+	message := timestamp + method + endpoint + string(body)
+	fmt.Println(message)
+
+	mac := hmac.New(sha256.New, []byte(api.secret))
+	mac.Write([]byte(message))
+	fmt.Println(mac)
+
+	sign := hex.EncodeToString(mac.Sum(nil)) // これは何をしてる？
+
+	return map[string]string {
+		"ACCESS-KEY": api.key,
+		"ACCESS-TIMESTAMP": timestamp,
+		"ACCESS-SIGN": sign,
+		"Content-Type" : "application/json",
+	}
+}
+
+// requestを投げる
+func (api *APIClient) doRequest(method, urlPath string, query [string]string, data []byte) (body[]byte, err error){
+	baseURL := url.Parse(baseURL)
+	if err := nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+
 }
